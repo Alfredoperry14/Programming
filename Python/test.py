bc44e79734480a9cdfa0a409c39aa9d8ade16753
Alfredoperry14 <alfredoperry14@gmail.com>
2022-03-31 18:58:54 -0700

    new input added

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,8 @@
 print('Hello, World!')
-print('What is your name')
-name = input()
+print('What is your first name')
+firstName = input()
+print('What is your last name')
+lastName = input()
+name = firstName+lastName
 
 print("Hello " + name +"! Wow, your name is " + str(len(name)) + " characters long!")
