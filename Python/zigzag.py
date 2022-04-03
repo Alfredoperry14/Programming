4f59b2ff0ad232c339fe7f616cb7b120d6bf60b2
Alfredoperry14 <alfredoperry14@gmail.com>
2022-04-02 18:25:29 -0700

    removed program

--- Python/zigzag.py
+++
@@ -1,8 +0,0 @@
-while(1):
-    manip = "***"
-    print(manip)
-    for i in range(10):
-        print(" "+manip)
-        print("  " + manip)
-        print("   " + manip)
-        print("  "+ manip)
